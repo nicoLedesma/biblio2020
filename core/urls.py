e98395d42bf05b2f9cb9c00d5129a309c55d80c4
Nicolas Ledesma <nicoledesma25@gmail.com>
2020-08-27 11:53:58 -0300

    Templates from the admin site

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from core.views import HomeView
 
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from core.models import MateriaLectura
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
+
